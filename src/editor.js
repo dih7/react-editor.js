6f5644b33e1b3a9ac44614a400d70585ca2941d5
Dan Hayes <danhayes13@gmail.com>
2019-08-23 17:56:47 +0100

    Update package.json

--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import EditorJS from '@editorjs/editorjs';
+import EditorJS from '@brandontle/editorjs';
 import Paragraph from '@editorjs/paragraph';
 import commonTools from './common-tools';
 
